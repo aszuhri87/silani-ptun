30e599be0cdced61e94886ac472fb243257164ea
Achmad Saefuddin Zuhri <achmad.s.zuhri182@gmail.com>
2022-06-10 09:25:41 +0700

    fix sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -59,17 +59,17 @@
             <li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="database"></i><span class="menu-title text-truncate" data-i18n="Menu Levels">Master Data</span></a>
                 <ul class="menu-content">
                     {{-- <li><a class="d-flex align-items-center" href="/applicant/mail"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level"></span></a> --}}
-                    <li><a class="d-flex align-items-center" href="/admin/unit"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Unit Bidang</span></a>
+                    <li class="nav-item @if (Request::is('admin/unit')) active @endif"><a class="d-flex align-items-center" href="/admin/unit"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Unit Bidang</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="/admin/sub-unit"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Sub Unit/Bidang</span></a>
+                    <li class="nav-item @if (Request::is('admin/sub-unit')) active @endif"><a class="d-flex align-items-center" href="/admin/sub-unit"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Sub Unit/Bidang</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="/admin/document-category"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Dokumen Kategori</span></a>
+                    <li class="nav-item @if (Request::is('admin/document-category')) active @endif"><a class="d-flex align-items-center" href="/admin/document-category"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Dokumen Kategori</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="/admin/req-type"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Tipe Keperluan</span></a>
+                    <li class="nav-item @if (Request::is('admin/req-type')) active @endif"><a class="d-flex align-items-center" href="/admin/req-type"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Tipe Keperluan</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="/admin/document-req"  data-toggle="tooltip"  data-bs-placement="right" title="Keperluan Dokumen"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Keperluan Dokumen</span></a>
+                    <li class="nav-item @if (Request::is('admin/document-req')) active @endif"><a class="d-flex align-items-center" href="/admin/document-req"  data-toggle="tooltip"  data-bs-placement="right" title="Keperluan Dokumen"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Keperluan Dokumen</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="/admin/document-category-req"  data-toggle="tooltip" data-bs-placement="right" title="Keperluan Dokumen Kategori"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Keperluan Dokumen Kategori</span></a>
+                    <li class="nav-item @if (Request::is('admin/document-category-req')) active @endif"><a class="d-flex align-items-center" href="/admin/document-category-req"  data-toggle="tooltip" data-bs-placement="right" title="Keperluan Dokumen Kategori"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Keperluan Dokumen Kategori</span></a>
                     </li>
                 </ul>
             </li>
@@ -77,11 +77,11 @@
             @role('admin')
             <li class="nav-item"><a class="d-flex align-items-center" href="#"><i data-feather="database"></i><span class="menu-title text-truncate" data-i18n="Menu Levels">Master Data</span></a>
                 <ul class="menu-content">
-                    <li><a class="d-flex align-items-center" href="/admin/document-category"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Dokumen Kategori</span></a>
+                    <li class="nav-item @if (Request::is('admin/document-category')) active @endif"><a class="d-flex align-items-center" href="/admin/document-category"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Dokumen Kategori</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="/admin/req-type"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Tipe Keperluan</span></a>
+                    <li class="nav-item @if (Request::is('admin/req-type')) active @endif"><a class="d-flex align-items-center" href="/admin/req-type"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Tipe Keperluan</span></a>
                     </li>
-                    <li><a class="d-flex align-items-center" href="/admin/document-category-req"  data-toggle="tooltip" data-bs-placement="right" title="Keperluan Dokumen Kategori"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Keperluan Dokumen Kategori</span></a>
+                    <li class="nav-item @if (Request::is('admin/document-category-req')) active @endif"><a class="d-flex align-items-center" href="/admin/document-category-req"  data-toggle="tooltip" data-bs-placement="right" title="Keperluan Dokumen Kategori"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Second Level">Keperluan Dokumen Kategori</span></a>
                     </li>
                 </ul>
             </li>
@@ -96,22 +96,22 @@
           @role('applicant')
             {{-- <li class=" nav-item"><a class="d-flex align-items-center" href="/applicant/notification"><i data-feather="bell"></i><span class="menu-title text-truncate" data-i18n="Chat">Notifikasi</span></a>
             </li> --}}
-            <li><a class="d-flex align-items-center" href="/applicant/document"><i data-feather="file-plus"></i><span class="menu-item text-truncate" data-i18n="Second Level">Buat Dokumen</span></a>
-            <li class="nav-item"><a class="d-flex align-items-center" href="/applicant/verification-process"><i data-feather="edit-3"></i><span class="menu-title text-truncate" >Perizinan Dokumen</span></a>
+            <li class="nav-item @if (Request::is('applicant/document')) active @endif"><a class="d-flex align-items-center" href="/applicant/document"><i data-feather="file-plus"></i><span class="menu-item text-truncate" data-i18n="Second Level">Buat Dokumen</span></a>
+            <li class="nav-item @if (Request::is('applicant/verification-process')) active @endif"><a class="d-flex align-items-center" href="/applicant/verification-process"><i data-feather="edit-3"></i><span class="menu-title text-truncate" >Perizinan Dokumen</span></a>
             </li>
 
-            <li class=" nav-item"><a class="d-flex align-items-center" href="/applicant/done-docs"><i data-feather="check-square"></i><span class="menu-title text-truncate" data-i18n="Todo">Selesai</span></a>
+            <li class="nav-item @if (Request::is('applicant/done-docs')) active @endif"><a class="d-flex align-items-center" href="/applicant/done-docs"><i data-feather="check-square"></i><span class="menu-title text-truncate" data-i18n="Todo">Selesai</span></a>
             </li>
            @endrole
 
 
            @hasrole('admin|super admin')
-            <li class="nav-item"><a class="d-flex align-items-center" href="/admin/inbox"><i data-feather="inbox"></i><span class="menu-title text-truncate" data-i18n="Chat">Dokumen Masuk</span></a>
+            <li class="nav-item @if (Request::is('admin/inbox')) active @endif"><a class="d-flex align-items-center" href="/admin/inbox"><i data-feather="inbox"></i><span class="menu-title text-truncate" data-i18n="Chat">Dokumen Masuk</span></a>
             </li>
-            <li class="nav-item"><a class="d-flex align-items-center" href="/admin/verification"><i data-feather="edit-3"></i><span class="menu-title text-truncate" >Perizinan Dokumen</span></a>
+            <li class="nav-item @if (Request::is('admin/verification')) active @endif"><a class="d-flex align-items-center" href="/admin/verification"><i data-feather="edit-3"></i><span class="menu-title text-truncate" >Perizinan Dokumen</span></a>
             </li>
 
-            <li class=" nav-item"><a class="d-flex align-items-center" href="/admin/accepted"><i data-feather="check-square"></i><span class="menu-title text-truncate" data-i18n="Todo">Selesai</span></a>
+            <li class="nav-item @if (Request::is('admin/accepted')) active @endif"><a class="d-flex align-items-center" href="/admin/accepted"><i data-feather="check-square"></i><span class="menu-title text-truncate" data-i18n="Todo">Selesai</span></a>
             </li>
             @endhasrole
             <li class=" navigation-header"><span data-i18n="Apps &amp; Pages">Akun &amp; Data</span><i data-feather="more-horizontal"></i>
@@ -119,17 +119,17 @@
 
 
           @hasrole('admin|super admin')
-            <li class=" nav-item"><a class="d-flex align-items-center" href="/admin/profile"><i data-feather="user"></i><span class="menu-title text-truncate" data-i18n="Documentation">Profile</span></a>
+            <li class="nav-item @if (Request::is('admin/profile')) active @endif"><a class="d-flex align-items-center" href="/admin/profile"><i data-feather="user"></i><span class="menu-title text-truncate" data-i18n="Documentation">Profile</span></a>
             </li>
         @endhasrole
 
         @role('applicant')
-            <li class=" nav-item"><a class="d-flex align-items-center" href="/applicant/profile"><i data-feather="user"></i><span class="menu-title text-truncate" data-i18n="Documentation">Profile</span></a>
+            <li class="nav-item @if (Request::is('applicant/profile')) active @endif"><a class="d-flex align-items-center" href="/applicant/profile"><i data-feather="user"></i><span class="menu-title text-truncate" data-i18n="Documentation">Profile</span></a>
             </li>
           @endrole
 
           @role('super admin')
-            <li class=" nav-item"><a class="d-flex align-items-center" href="/admin/manage-admin"><i data-feather="user-plus"></i><span class="menu-title text-truncate" data-i18n="Documentation">Manajemen Admin</span></a>
+            <li class="nav-item @if (Request::is('admin/manage-admin')) active @endif"><a class="d-flex align-items-center" href="/admin/manage-admin"><i data-feather="user-plus"></i><span class="menu-title text-truncate" data-i18n="Documentation">Manajemen Admin</span></a>
             </li>
             @endrole
             <li class=" nav-item"><a class="d-flex align-items-center" href="{{ route('logout') }}" onclick="event.preventDefault();
