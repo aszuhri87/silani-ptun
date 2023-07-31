3232f99d4ab7cf94e6667e537af993a3d1bd0c08
aszuhri87 <achmad.zuhri@varx.id>
2023-07-31 09:42:56 +0700

    fix: disposisi

--- resources/views/applicant/disposition/modal.blade.php
+++ resources/views/applicant/disposition/modal.blade.php
@@ -176,31 +176,31 @@
                                         <p class="no-space forward-1" style="float: left;"></p>
                                     </div>
                                     <div style="display: flex;">
-                                        <p class="no-space">3. Panitera</p>
+                                        <p class="no-space">2. Panitera</p>
                                         <p class="no-space forward-2" style="float: left;"></p>
                                     </div>
                                     <div style="display: flex;">
-                                        <p class="no-space">4. Sekretaris</p>
+                                        <p class="no-space">3. Sekretaris</p>
                                         <p class="no-space forward-3" style="float: left;"></p>
                                     </div>
                                     <div style="display: flex;">
-                                        <p class="no-space">5. Panitera Muda Hukum</p>
+                                        <p class="no-space">4. Panitera Muda Hukum</p>
                                         <p class="no-space forward-4" style="float: left;"></p>
                                     </div>
                                     <div style="display: flex;">
-                                        <p class="no-space">6. Panitera Muda Perkara</p>
+                                        <p class="no-space">5. Panitera Muda Perkara</p>
                                         <p class="no-space forward-5" style="float: left;"></p>
                                     </div>
                                     <div style="display: flex;">
-                                        <p class="no-space">7. Kasub Umum dan Keuangan</p>
+                                        <p class="no-space">6. Kasub Umum dan Keuangan</p>
                                         <p class="no-space forward-6" style="float: left;"></p>
                                     </div>
                                     <div style="display: flex;">
-                                        <p class="no-space">8. Kasub Kepegawaian, Ortala</p>
+                                        <p class="no-space">7. Kasub Kepegawaian, Ortala</p>
                                         <p class="no-space forward-7" style="float: left;"></p>
                                     </div>
                                     <div style="display: flex;">
-                                        <p class="no-space">9. Kasub Umum dan Keuangan</p>
+                                        <p class="no-space">8. Kasub Umum dan Keuangan</p>
                                         <p class="no-space forward-8" style="float: left;"></p>
                                     </div>
                                 </td>
